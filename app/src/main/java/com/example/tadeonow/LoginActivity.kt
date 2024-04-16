6482c41ee84e4676673ccfc97d232502fc2f4a61
JHONATANGold <jhonatan.munozfmm@gmail.com>
2024-04-16 02:40:30 -0500

    Septimo commit

--- app/src/main/java/com/example/tadeonow/LoginActivity.kt
+++ app/src/main/java/com/example/tadeonow/LoginActivity.kt
@@ -8,13 +8,14 @@ import android.widget.Button
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        val selfChange = null
+        super.onCreate(selfChange)
         setContentView(R.layout.activity_login)
 
         val entrarButton = findViewById<Button>(R.id.Bt_entrar)
 
         entrarButton.setOnClickListener {
-            val intent = Intent(this, Page1Activity::class.java) // Assuming Page1Activity exists
+            val intent = Intent(this, Page1::class.java) // Assuming Page1Activity exists
             startActivity(intent)
         }
     }
