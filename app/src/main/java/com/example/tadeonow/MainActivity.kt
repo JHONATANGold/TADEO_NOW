de881adbe2329dae768d1ca83ac5373f868eb6d1
JHONATANGold <jhonatan.munozfmm@gmail.com>
2024-04-15 19:36:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tadeonow/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tadeonow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val Button = btn1,btn2;
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
