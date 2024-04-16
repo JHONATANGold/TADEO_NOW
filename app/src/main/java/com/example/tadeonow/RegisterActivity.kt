73f181c0f05451462cb3061425986b5076a8abfb
JHONATANGold <jhonatan.munozfmm@gmail.com>
2024-04-16 01:05:53 -0500

    Cuarto commit

--- app/src/main/java/com/example/tadeonow/RegisterActivity.kt
+++ app/src/main/java/com/example/tadeonow/RegisterActivity.kt
@@ -2,10 +2,19 @@ package com.example.tadeonow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val volverButton = findViewById<Button>(R.id.Bt_volver)
+
+        volverButton.setOnClickListener {
+            // Finish this activity to go back to MainActivity
+            finish()
+        }
     }
 }
